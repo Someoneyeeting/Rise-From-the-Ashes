043ac0ab011802ec066a4ccdc877cb1300f1bb21
mahmoudElshrif <mahmoudfathyelshrif@gmail.com>
2024-12-09 05:30:18 +0200

    rise and shine

---
+++ player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+
+@export
+var speed : float = 20
+
+@export
+var jumpheight : float = 100
+
+@export
+var rising_speed : float = 10
+
+
+func normal_move():
+	var move := Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	velocity.y += 10.3
+	if(is_on_floor()):
+		if(velocity.y > 0):
+			velocity.y = 0
+	
+	if(Input.is_action_just_pressed("up")):
+		velocity.y = -jumpheight
+	
+	if(Input.is_action_just_pressed("rise")):
+		rise()
+	
+	velocity.x = move * speed
+	
+
+func rise():
+	$rising.start()
+	velocity.y = -rising_speed
+
+func rising():
+	velocity.x = 0
+	velocity.y += 30
+	#velocity.y = -rising_speed * ($rising.time_left / $rising.wait_time)
+
+func _physics_process(delta: float) -> void:
+	$flames.emitting = not $rising.is_stopped()
+	if($rising.is_stopped()):
+		normal_move()
+	else:
+		rising()
+	move_and_slide()
